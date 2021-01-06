545624c9cc8de2a45e782233acd8f93bff5094a3
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2021-01-06 12:39:36 +0100

    Create send.py

---
+++ send.py
@@ -0,0 +1,18 @@
+import smtplib
+import creds
+
+sender_email= "sender@gmail.com"
+receiver_email = "receiver@gmail.com"
+
+password = creds.password
+
+message =" This is a test email sent with python"
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+
+server.starttls()
+server.login(sender_email, password)
+print('Login succesfull')
+server.sendmail(sender_email, receiver_email, message)
+print('Your email has been sent to', receiver_email)
+    
